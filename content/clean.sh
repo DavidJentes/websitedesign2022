5393c4a536faf1479d16f18d46d2f96a1fe87dfa
DavidDGTNT <david@dgtntstudios.tk>
2022-02-11 01:05:45 +0000

    Remove the whole content-generation system since I'm just going to use HTML

--- content/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CONTENT[0]="about"
-CONTENT[1]="book1"
-CONTENT[2]="book2"
-
-for i in {0..2}
-do
-    rm "${CONTENT[i]}.html"
-done
--- content/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CONTENT[0]="about"
-CONTENT[1]="book1"
-CONTENT[2]="book2"
-
-for i in {0..2}
-do
-    markdown "${CONTENT[i]}.md" > "${CONTENT[i]}.html"
-done
